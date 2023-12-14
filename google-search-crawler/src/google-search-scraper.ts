ac1fd00207b5e9aa6bae7c1f9879565a98b617ea
Kevin <guyan.data@gmail.com>
2023-12-15 01:54:14 +0800

    update

--- google-search-crawler/src/google-search-scraper.ts
+++ google-search-crawler/src/google-search-scraper.ts
@@ -1,20 +1,57 @@
 // For more information, see https://crawlee.dev/
-import { PlaywrightCrawler } from "crawlee";
+import { PlaywrightCrawler, RequestQueue } from "crawlee";
 
-interface SearchResult {
-  url: string;
-  title: string;
-  relatedKeywords: string[];
-  organicSearchResults: any;
+interface faq {
+  question: string;
+  answer: string;
+  url: string | null | undefined;
+  title: string | null | undefined;
 }
 
-export async function ScrapeGoogleSearch(url: string[]) {
+interface featuredSnippets {
+  url: string | null | undefined;
+  title: string | null | undefined;
+  content: string[];
+}
+
+interface organicResult {
+  title: string;
+  url: string;
+  desc: string;
+  emphasizedKeywords?: string[];
+  posistion: number;
+}
+
+interface SearchResult {
+  resultTotal: number;
+  url: string;
+  title: string;
+  featuredSnippets: featuredSnippets;
+  peopleAlsoAsk: faq[];
+  relatedQueries: string[];
+  organicResults: organicResult[];
+}
+
+export async function ScrapeGoogleSearch(urls: string[]) {
+  // 创建一个新的请求队列实例
+  const queueId = `queue-${Date.now()}`;
+  const requestQueue = await RequestQueue.open(queueId);
+
+  // 为每个 URL 添加请求到队列
+  for (const url of urls) {
+    await requestQueue.addRequest({ url });
+  }
+
   const results: SearchResult[] = [];
   const crawler = new PlaywrightCrawler({
-    // maxRequestsPerCrawl: 1,
+    requestQueue,
     async requestHandler({ request, page }) {
       console.log(`Processing ${request.url}...`);
       const title = await page.title();
+
+      //results total
+      const resultTotal = await page.textContent("#result-stats");
+      const resultTotalNumber = extractResultTotal(resultTotal || "") || 0;
 
       //related keywords
       let elements = await page.$$(".EIaa9b a");
@@ -26,29 +63,127 @@ export async function ScrapeGoogleSearch(url: string[]) {
         }
       }
 
+      //featured snippets
+      let featuredSnippets = {
+        content: [] as string[],
+        url: "",
+        title: "",
+      };
+      const featuredSnippetsSection = await page.$(".ULSxyf");
+      if (featuredSnippetsSection) {
+        const featuredUrlEle = await featuredSnippetsSection?.$(".yuRUbf a");
+        const featuredUrl = (await featuredUrlEle?.getAttribute("href")) || "";
+        const featuredTitleEle = await featuredSnippetsSection?.$(".yuRUbf h3");
+        const featuredTitle = (await featuredTitleEle?.textContent()) || "";
+
+        elements = await featuredSnippetsSection.$$(".di3YZe li");
+        const snippets = [];
+        for (const element of elements) {
+          const featuredSnippetContent = await element.textContent();
+          if (featuredSnippetContent) {
+            snippets.push(featuredSnippetContent);
+          }
+        }
+
+        featuredSnippets = {
+          content: snippets,
+          url: featuredUrl,
+          title: featuredTitle,
+        };
+      }
+
+      //faq
+      const faqResults: faq[] = [];
+      // 直接选择包含 "People also ask" 的区域
+      const faqSection = await page.$(".MjjYud:has-text('People also ask')");
+      if (faqSection) {
+        // 选择该区域内的问题
+        const faqsEle = await faqSection.$$(".wQiwMc");
+        for (const faqEle of faqsEle) {
+          const questionEle = await faqEle.$(".JlqpRe span");
+          const question = await questionEle?.textContent();
+          const answerEle = await faqEle.$(".wDYxhc span");
+          const answer = await answerEle?.textContent();
+          const urlEle = await faqEle.$(".yuRUbf a");
+          const url = await urlEle?.getAttribute("href");
+          const titleEle = await faqEle.$(".yuRUbf h3");
+          const title = await titleEle?.textContent();
+          if (question && answer) {
+            const faqObj: faq = { question, answer, url, title };
+            faqResults.push(faqObj);
+          }
+        }
+      }
+
       //organic search results
-      elements = await page.$$(".MjjYud a");
-      const organicSearchResults: string[] = [];
-      for (const element of elements) {
-        const organicSearchResult = await element.textContent();
-        if (organicSearchResult) {
-          organicSearchResults.push(organicSearchResult);
+      const organicResults: organicResult[] = [];
+      const organicSection = await page.$$(".MjjYud");
+      if (organicSection) {
+        for (const element of organicSection) {
+          const urlEle = await element.$("a");
+          const url = await urlEle?.getAttribute("href");
+          const titleEle = await element.$("h3");
+          const title = await titleEle?.textContent();
+          const descEle = await element.$(
+            ".VwiC3b.yXK7lf.lVm3ye.r025kc.hJNv6b.Hdw6tb"
+          );
+          const desc = await descEle?.textContent();
+          const emList: string[] = [];
+          const emEles = await descEle?.$$("em");
+          if (emEles) {
+            for (const element of emEles) {
+              const em = await element.textContent();
+              if (em) {
+                emList.push(em.toLocaleLowerCase());
+              }
+            }
+          }
+          const posistion = organicResults.length + 1;
+          if (url && title && desc) {
+            const organicResultObj: organicResult = {
+              url,
+              title,
+              desc,
+              emphasizedKeywords: Array.from(new Set(emList)),
+              posistion,
+            };
+            organicResults.push(organicResultObj);
+          }
         }
       }
 
       results.push({
+        resultTotal: resultTotalNumber,
         url: request.url,
         title,
-        relatedKeywords,
-        organicSearchResults,
+        featuredSnippets: featuredSnippets,
+        peopleAlsoAsk: faqResults,
+        relatedQueries: relatedKeywords,
+        organicResults,
       });
     },
   });
 
   try {
-    await crawler.run(url);
+    await crawler.run(urls);
     return results;
   } catch (error) {
     throw error;
   }
+}
+
+function extractResultTotal(str: string): number | null {
+  // 定义一个正则表达式，匹配包含逗号的数字
+  const regex = /(\d{1,3}(,\d{3})*(\.\d+)?)|(\d+)/;
+  const match = str.match(regex);
+
+  // 如果匹配到数字，则返回转换后的数字
+  if (match) {
+    // 移除数字中的逗号
+    const numberString = match[0].replace(/,/g, "");
+    return parseFloat(numberString);
+  }
+
+  // 如果没有匹配到，则返回 null
+  return null;
 }
